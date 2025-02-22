965f9cd6312e39023c375da1de93617ab137cc22
JesusBlan <jezublanco@gmail.com>
2025-02-22 12:33:29 +0100

    pseudocodedthe comcept of the algoritm for a better overall view of the algorithm

---
+++ index.js
@@ -0,0 +1,6 @@
+
+//divide and conquer! we need to split the array in two equal parts, left and rigth
+//sort left part
+//sort rigth part
+//do it until the array left has one element inside, si its already sorted
+//merge them in order, comparing the first element in left part and rigth part of each spliced array
