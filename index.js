6387b3fd071db83b95c99f8f88febc0f44da7bd5
JesusBlan <jezublanco@gmail.com>
2025-02-22 12:49:39 +0100

    added a link to a Harvard lesson who made it clear for me

--- index.js
+++ index.js
@@ -4,3 +4,4 @@
 //sort rigth part
 //do it until the array left has one element inside, si its already sorted
 //merge them in order, comparing the first element in left part and rigth part of each spliced array
+//CS50x Harvard lesson https://www.youtube.com/watch?v=Ns7tGNbtvV4 after a couple of hours looking for resources I can assure you this is the holy grail of merge sort explanations
